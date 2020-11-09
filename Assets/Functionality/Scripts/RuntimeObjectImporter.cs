c0e2b2026c0fe3b6e87d241b0191b7aecb83ca86
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-11-09 12:29:56 -0500

    - removed pixyz

--- Assets/Functionality/Scripts/RuntimeObjectImporter.cs
+++ Assets/Functionality/Scripts/RuntimeObjectImporter.cs
@@ -1,33 +1,33 @@
 ﻿
-using Pixyz.Import;
+//using Pixyz.Import;
 using UnityEngine;
 
 public class RuntimeObjectImporter : MonoBehaviour
 {
 
-    public void ImportModel()
-    {
-        string file = @"C:\Users\CJMCassar\Downloads\bugatti-chiron-6.snapshot.4\CHIRON2016.3dm";
-        ImportSettings importSettings = ScriptableObject.CreateInstance<ImportSettings>();
-        Importer importer = new Importer(file, importSettings);
-        importer.progressed += progressed;
-        importer.completed += completed;
-        importer.run();
-        Debug.Log("ImportModel class started");
-    }
+    //public void ImportModel()
+    //{
+    //    string file = @"C:\Users\CJMCassar\Downloads\bugatti-chiron-6.snapshot.4\CHIRON2016.3dm";
+    //    ImportSettings importSettings = ScriptableObject.CreateInstance<ImportSettings>();
+    //    Importer importer = new Importer(file, importSettings);
+    //    importer.progressed += progressed;
+    //    importer.completed += completed;
+    //    importer.run();
+    //    Debug.Log("ImportModel class started");
+    //}
 
-    private void progressed(float progress, string message)
-    {
-        Debug.Log("Progress : " + 100f * progress + "%");
-    }
+    //private void progressed(float progress, string message)
+    //{
+    //    Debug.Log("Progress : " + 100f * progress + "%");
+    //}
 
-    private void completed(GameObject gameObject)
-    {
-        Debug.Log("Model Imported");
-    }
+    //private void completed(GameObject gameObject)
+    //{
+    //    Debug.Log("Model Imported");
+    //}
 
-    private void onFailed(ExceptionHandler exception)
-    {
-        Debug.Log("Failed ! Error : " + exception);
-    }
+    //private void onFailed(ExceptionHandler exception)
+    //{
+    //    Debug.Log("Failed ! Error : " + exception);
+    //}
 }
--- Assets/Plugins/Pixyz/Editor/Menus/SingletonEditorWindow.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-
-namespace Pixyz.Editor
-{
-    public abstract class SingletonEditorWindow : EditorWindow
-    {
-        public abstract string WindowTitle { get; }
-    }
-}
