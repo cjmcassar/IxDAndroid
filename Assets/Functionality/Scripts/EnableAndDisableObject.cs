503505d8585e17609a539b2316b8d867723da11a
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-11-09 21:53:28 -0500

    - added a ui messager for the app.

---
+++ Assets/Functionality/Scripts/EnableAndDisableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAndDisableObject : MonoBehaviour
+{
+    public GameObject ChatWindow;
+
+    public void Enable()
+    {
+        ChatWindow.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        ChatWindow.SetActive(false);
+    }
+
+}
